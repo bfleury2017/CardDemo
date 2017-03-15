7f879372c5fbf0f63fcaddc9bace16146b8a0e17
bfleury2017 <bfleury@outlook.com>
2017-03-14 20:41:28 -0400

    After Collapsing Toolbar color

--- app/src/main/java/com/nhti/carddemo/CardDemoActivity.java
+++ app/src/main/java/com/nhti/carddemo/CardDemoActivity.java
@@ -1,13 +1,12 @@
 package com.nhti.carddemo;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -22,6 +21,11 @@ public class CardDemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card_demo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+
+        collapsingToolbarLayout.setTitle("My toolbar title");
+        collapsingToolbarLayout.setContentScrimColor(Color.GREEN);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         layoutManager = new LinearLayoutManager(this);
